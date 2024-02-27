2ca85ca382309f2c5d14d8872ff935107ef1f6b4
RosiNantis <alexandros.samartzis1986@gmail.com>
2024-02-27 18:09:48 +0100

    fixing models

--- db/db_setup.py
+++ db/db_setup.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "user:alexandros@localhost/alexandros.samartzis"
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres@localhost/fast_lms"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}, future =True
